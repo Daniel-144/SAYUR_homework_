55bb2efda9d5494657c6ad713c477c6777040015
Daniel-144 <dani0142004@gmail.com>
2023-09-17 20:20:23 +0530

    assignment

--- string_function/password.py
+++ string_function/password.py
@@ -9,7 +9,7 @@
 """
 # initializing list to count the numbert of special characters and number
 numbers=['1','2','3','4','5','6','7','8','9','0']
-specialChar=['~','`','!','@','#','$','%','^','&','*','?','<','>',':',';','|','/']
+specialChar=['~','`','!','@','#','$','%','^','&','*','?','<','>',':',';','|','/','{','}']
 # loop to get a strong password( loop will execute until the user enters a strong password)
 while(True):
     # counter for number
@@ -21,12 +21,15 @@ while(True):
     # password should not contain space so we use if statement to reject the password with space
     if " " in password:
         print("password should not contain a space")
+        print("try again")
     # average minimum password length.
     if len(password) < 8:
         print("password is too short")
+        print("try again")
     # average maximum password length
     elif len(password) > 127:
         print("password is too long")
+        print("try again")
     else:
         # loop to count the number of special character
         for i in password:
@@ -35,6 +38,7 @@ while(True):
         # if statement to check if the password contains only one type of character(character or integer or special character).
         if password.isalpha() == True or password.isdigit() == True or specialCharCount==len(password):
             print("Password strength:'weak'")
+            print("try again")
         else:
             # loop to count the number of integers present in the password
             for char in password:
@@ -45,6 +49,7 @@ while(True):
             # determing the strength of the characters
             if specialCharCount==0:
                 print('password strength:weak')
+                print("try again")
             if charCount >=1 and numCount ==1 and specialCharCount == 1:
                 print("password strength:Ok")
             elif charCount>=3 and numCount>=2 and specialCharCount>=1:
