8b3c8ffc66e2c502916d1ec5e294bbf0383fb634
Daniel-144 <dani0142004@gmail.com>
2023-10-02 21:37:28 +0530

    assignment

--- collatz_conjecture/collatz_conjecture.py
+++ collatz_conjecture/collatz_conjecture.py
@@ -12,5 +12,5 @@ while(number != 1):
     if ( number % 2 == 0): 
         number = number//2  # for even number , divide by 2(n/2)
     else:
-        number=(3*number)+1
+        number=(3*number)+1 # for odd number , 3n+1
 print(f"number became {number} after {count} iterations")
