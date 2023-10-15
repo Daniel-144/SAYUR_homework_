0984f7164e98cd769e3d53081914fdca39cec619
Daniel-144 <dani0142004@gmail.com>
2023-10-15 19:58:50 +0530

    assignment

--- dictionary/cafe_updated.py
+++ dictionary/cafe_updated.py
@@ -5,17 +5,46 @@ You are running a cafe. Write a program (only the functions with input and outpu
 import re
 
 menu={
-    "vadai": { "stock": 50, "sellingprice": 15, "costprice":10, "sold":0},
-    "coffee": {"stock": 50, "sellingprice": 17, "costprice":12, "sold":0},
-    "tea":{ "stock": 50, "sellingprice": 17, "costprice":12, "sold":0},
-    "samosa":{"stock": 50, "sellingprice": 20, "costprice":12, "sold":0},
+    "vadai": { "stock": 100, "sellingprice": 15, "costprice":10, "sold":0},
+    "coffee": {"stock": 100, "sellingprice": 17, "costprice":12, "sold":0},
+    "tea":{ "stock": 100, "sellingprice": 17, "costprice":12, "sold":0},
+    "samosa":{"stock": 100, "sellingprice": 20, "costprice":12, "sold":0},
 }
-order = input("What you need:")
-# Use regular expression to extract numbers
-numbers = re.findall(r'(\d+)\s+(?:coffee|tea|biscuit|vadai)', order)
-items = re.findall(r'\b(coffees?|teas?|vadais?|biscuits?)\b', order)
-bill = 0
+expenses = 0
+earned=0
 # loop to calculate the expenses
-for items,details in menu.items():
-    print(f"{items}\t: {details['sellingprice']}")
+for customers in range(1,11):
+    bill=0
+    for products,details in menu.items():
+        print(f"{products}\t: {details['sellingprice']} RS")
+        expenses+= details["costprice"]
+    order = input("What you need:")
+    # Use regular expression to extract numbers
+    quantity = re.findall(r'(\d+)\s+(?:coffee|tea|samosa|vadai)', order)
+    items = re.findall(r'\b(coffees?|teas?|vadais?|samosas?)\b', order)
+    for item in range(len(items)):
+        if int(quantity[item])<menu[items[item]]["stock"]:
+            menu[items[item]]["stock"] -= int(quantity[item])
+            menu[items[item]]["sold"] += int(quantity[item])
+        else:
+            print(f"only {menu[items[item]]['stock']} {items[item]} left")
+    print("\tBILL\t")
+    for product in range(len(items)):
+        print(f"{items[product]} X {quantity[product]}\t:{int(quantity[product])*menu[items[product]]['sellingprice']}")
+        bill += int(quantity[product]) * menu[items[product]]['sellingprice']
+    print(f"{'*'*10}")
+    print(f"Total:{bill}\n")
+
+for products,details in menu.items():
+    earned += details["sellingprice"] * ["sold"]
+    
+if earned > expenses:
+    print(f"profit:{earned-expenses}")
+elif earned < expenses:
+    print(f"loss:{earned-expenses}")
+else:
+    print("neither profit nor loss.")
+    
+
+
 
